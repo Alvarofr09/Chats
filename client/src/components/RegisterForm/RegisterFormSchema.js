1c5c25823d1a317a8a7cd85087ebda88d92b806e
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-18 12:01:25 +0200

    Schema del formulario de Registro

---
+++ client/src/components/RegisterForm/RegisterFormSchema.js
@@ -0,0 +1,14 @@
+import * as yup from "yup";
+
+export const RegisterFormSchema = yup.object().shape({
+	username: yup.string().min(5).required("Username is required"),
+	email: yup.string().email("Invalid email").required("Email Required"),
+	password: yup.string().min(5).required("Password Required"),
+	confirmPassword: yup
+		.string()
+		.oneOf([yup.ref(`password`)], "Passwords must match")
+		.required("Confirm Password Required"),
+	acceptedTC: yup
+		.boolean()
+		.oneOf([true], "Please accept the terms and conditions"),
+});
