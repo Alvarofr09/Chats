b6605e4d14373582cc7de3c615e5b0a18011e0c0
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-18 13:27:39 +0200

    comentar label

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -4,13 +4,16 @@ export default function Input({ label, ...props }) {
 	const [field, meta] = useField(props);
 	return (
 		<>
-			<label>{label}</label>
+			{/* <label className="text-blue-500 text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#e8e8e8] w-fit">
+				{label}
+			</label> */}
 			<input
 				{...props}
 				{...field}
 				className={
 					meta.touched && meta.error ? "input-error" : "register-input"
 				}
+				// className="border-blue-500 input px-[10px] py-[11px] text-xs bg-[#e8e8e8] border-2 rounded-[5px] focus:outline-none placeholder:text-black/25"
 			/>
 			{meta.touched && meta.error && <div className="error">{meta.error}</div>}
 		</>
