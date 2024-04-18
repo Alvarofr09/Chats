f0ef320a3eb2fbb4793e29b4a2dcc2517be12cb5
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-18 09:17:00 +0200

    Componente checkbox

---
+++ client/src/components/ui/Checkbox.jsx
@@ -0,0 +1,17 @@
+import { useField } from "formik";
+
+export default function Checkbox({ ...props }) {
+	const [field, meta] = useField(props);
+
+	return (
+		<div>
+			<input
+				{...props}
+				{...field}
+				className={meta.touched && meta.error ? "input-error" : ""}
+			/>
+			<span>Accept the terms and conditions</span>
+			{meta.touched && meta.error && <div className="error">{meta.error}</div>}
+		</div>
+	);
+}
