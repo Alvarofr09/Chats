79947eed0a84fb9a09397e6d693e981a6bfdb1b8
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-18 12:00:33 +0200

    Ruta de prueba

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./views/Register";
 import Chat from "./views/Chat";
 import Login from "./views/Login";
+import Prueba from "./views/Prueba";
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/register" element={<Register />} />
+				<Route path="/prueba" element={<Prueba />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/" element={<Chat />} />
 			</Routes>
