f414bbba869de1e613e286385213db084b97a42f
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-18 12:01:58 +0200

    Comentar axios

--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
+// import axios from "axios";
 
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
