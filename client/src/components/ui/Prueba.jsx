0dcec44637dc21af6cb4e82d3d73bbc259f02ec1
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-19 09:32:16 +0200

    Hcer bien el input de prueba

--- client/src/components/ui/Prueba.jsx
+++ client/src/components/ui/Prueba.jsx
@@ -1,20 +1,28 @@
-export default function InputPrueba() {
+import { useField } from "formik";
+
+export default function InputPrueba({ label, ...props }) {
+	const [field, meta] = useField(props);
 	return (
-		<label className="relative">
-			<input
-				type="text"
-				className="h-20 w-96 px-6 text-4xl bg-black border-2 
-                rounded-lg border-white border-opacity-50 outline-none
-                focus:border-b-sky-800 focus:text-white
-                transition duration-200 "
-			/>
-			<span
-				className="text-4xl text-white text-opacity-80 absolute left-0 top-4
+		<>
+			<label className="relative">
+				<input
+					{...props}
+					{...field}
+					className={
+						meta.touched && meta.error
+							? "input-error"
+							: "h-20 w-96 px-6 text-4xl bg-black border-2 rounded-lg border-white border-opacity-50 outline-none focus:border-sky-500 focus:text-white transition duration-200 "
+					}
+				/>
+				<span
+					className="text-4xl text-white text-opacity-80 absolute left-0 top-4
                 mx-6 px-2 transition duration-200
                 input-text"
-			>
-				Input
-			</span>
-		</label>
+				>
+					{label}
+				</span>
+			</label>
+			{meta.touched && meta.error && <div className="error">{meta.error}</div>}
+		</>
 	);
 }
