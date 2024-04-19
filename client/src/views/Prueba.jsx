66ef9f16f123ad0ddc6de27ea299196229abca9b
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-19 09:32:28 +0200

    Probar el input de prueba

--- client/src/views/Prueba.jsx
+++ client/src/views/Prueba.jsx
@@ -4,8 +4,8 @@ import InputPrueba from "../components/ui/Prueba";
 export default function Prueba() {
 	return (
 		<div className="bg-black h-screen flex justify-center items-center">
-			<InputPrueba />
-			{/* <RegisterForm /> */}
+			{/* <InputPrueba label="Username" name="username" type="text" /> */}
+			<RegisterForm />
 		</div>
 	);
 }
