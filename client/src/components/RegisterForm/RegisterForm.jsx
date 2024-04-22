69a05929d33bdca0d05b7ddb4280d2c240569991
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-04-22 12:30:32 +0200

    Usar el toastify, y hacer las cosas con el input bonito

--- client/src/components/RegisterForm/RegisterForm.jsx
+++ client/src/components/RegisterForm/RegisterForm.jsx
@@ -1,85 +1,86 @@
 import { Formik, Form } from "formik";
 import { initialValues } from "./InitialValues";
+import { ToastContainer, toast } from "react-toastify";
 import { RegisterFormSchema } from "./RegisterFormSchema";
-
-import InputPrueba from "../ui/Prueba";
 
 import Logo from "../../assets/logo.svg";
 
-import Input from "../ui/Input";
+import InputPrueba from "../ui/Prueba";
 import Checkbox from "../ui/Checkbox";
-import { Link } from "react-router-dom";
-
-async function onSubmit(values, actions) {
-	console.log(values);
-	console.log(actions);
-
-	await new Promise((resolve) => setTimeout(resolve, 1000));
-	actions.resetForm();
-}
+import { Link, useNavigate } from "react-router-dom";
+// import { useState } from "react";
+import { registerRoute, userApi } from "../../api/APIRoutes";
 
 export default function RegisterForm() {
+	const navigate = useNavigate();
+	const toastOptions = {
+		position: "bottom-right",
+		autoClose: 5000,
+		pauseOnHover: true,
+		draggable: true,
+		theme: "dark",
+	};
+
+	async function onSubmit(values) {
+		const { username, email, password } = values;
+		const { data } = await userApi.post(registerRoute, {
+			username,
+			email,
+			password,
+		});
+
+		if (data.status === false) {
+			toast.error(data.msg, toastOptions);
+		} else {
+			localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+			toast.success(data.msg, toastOptions);
+			navigate("/");
+		}
+	}
 	return (
-		<Formik
-			initialValues={initialValues}
-			validationSchema={RegisterFormSchema}
-			onSubmit={onSubmit}
-		>
-			{(values, errors, isSubmitting) => (
-				<div className="container-form">
-					<Form className="register-form">
-						<div className="brand ">
-							<img src={Logo} alt="Logo" className="h-20" />
-							<h1 className="titulo">Tradeium</h1>
-						</div>
-						<Input
-							// label="Username"
-							name="username"
-							type="text"
-							placeholder="Enter your username"
-						/>
-						<Input
-							// label="Email"
-							name="email"
-							type="email"
-							placeholder="Enter your email"
-						/>
-						<Input
-							// label="Password"
-							name="password"
-							type="password"
-							placeholder="Enter your password"
-						/>
-						<InputPrueba
-							label="Username"
-							name="username"
-							type="text"
-							// placeholder="Enter your username"
-						/>
-						<Input
-							// label="Confirm Password"
-							name="confirmPassword"
-							type="password"
-							placeholder="Confirm your password"
-						/>
-						<Checkbox type="checkbox" name="acceptedTC" />
-						<button
-							className="register-button"
-							type="submit"
-							disabled={isSubmitting}
-						>
-							Register
-						</button>
-						<span className="titulo">
-							Already have an account ?{" "}
-							<Link className="link" to="/login">
-								Login
-							</Link>
-						</span>
-						{/* <pre>{JSON.stringify({ values, errors }, null, 1)}</pre> */}
-					</Form>
-				</div>
-			)}
-		</Formik>
+		<>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={RegisterFormSchema}
+				onSubmit={onSubmit}
+			>
+				{(values, errors, isSubmitting) => (
+					<div className="container-form">
+						<Form className="register-form">
+							<div className="brand ">
+								<img src={Logo} alt="Logo" className="h-20" />
+								<h1 className="titulo">Tradeium</h1>
+							</div>
+
+							<InputPrueba label="Username" name="username" type="text" />
+
+							<InputPrueba label="Email" name="email" type="email" />
+
+							<InputPrueba label="Password" name="password" type="password" />
+							<InputPrueba
+								label="Confirm Password"
+								name="confirmPassword"
+								type="password"
+							/>
+							<Checkbox type="checkbox" name="acceptedTC" />
+							<button
+								className="register-button"
+								type="submit"
+								disabled={isSubmitting}
+							>
+								Register
+							</button>
+							<span className="titulo">
+								Already have an account ?{" "}
+								<Link className="link" to="/login">
+									Login
+								</Link>
+							</span>
+						</Form>
+					</div>
+				)}
+			</Formik>
+			<ToastContainer />
+		</>
 	);
 }
