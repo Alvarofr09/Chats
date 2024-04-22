6aa5f27c883c452084e3c62b6da7d3ae8cb23c1b
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-22 23:11:51 +0200

    Hacer el contendor de los chats

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -1,11 +1,13 @@
-import { useAuthContext } from "../context/AuthContext";
-
 export default function Chat() {
-	const { logout } = useAuthContext();
 	return (
-		<div>
-			<h1>Chat</h1>
-			<button onClick={logout}>Logout</button>
+		<div
+			className="h-screen w-screen flex flex-col justify-center gap-4
+		items-center bg-[#131324] "
+		>
+			<div
+				className="container h-[85%] w-[85%] bg-[#00000076]
+			grid grid-cols-[25% 75%] lg:grid-cols-[35% 65%]"
+			></div>
 		</div>
 	);
 }
