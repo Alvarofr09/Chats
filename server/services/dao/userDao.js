7f25741202041998491396a00234e47ab3dafbb6
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-22 20:23:18 +0200

    funcion getuserById, set Avatar y updateUser

--- server/services/dao/userDao.js
+++ server/services/dao/userDao.js
@@ -2,8 +2,27 @@ const moment = require("moment");
 const db = require("../db");
 
 const md5 = require("md5");
+const { removeUndefinedKeys } = require("../../utils/removeUndefinedKeys");
 
 const userDao = {};
+
+userDao.getUserById = async (id) => {
+	let conn = null;
+	try {
+		conn = await db.createConection();
+
+		return await db.query(
+			"SELECT * FROM users WHERE id = ?",
+			[id],
+			"select",
+			conn
+		);
+	} catch (error) {
+		throw new Error(error);
+	} finally {
+		conn && (await conn.end());
+	}
+};
 
 userDao.getUserByUsername = async (username) => {
 	let conn = null;
@@ -59,4 +78,54 @@ userDao.createUser = async (userData) => {
 		conn && (await conn.end());
 	}
 };
+
+userDao.setAvatar = async (id, avatarImage) => {
+	let conn = null;
+	try {
+		conn = await db.createConection();
+
+		return await db.query(
+			"UPDATE users SET avatarImage = ? WHERE id = ?",
+			[avatarImage, id],
+			"update",
+			conn
+		);
+	} catch (error) {
+		throw new Error(error);
+	} finally {
+		conn && (await conn.end());
+	}
+};
+
+userDao.updateUser = async (id, userData) => {
+	let conn = null;
+	try {
+		conn = await db.createConection();
+
+		let userObj = {
+			name: userData.name,
+			surname: userData.surname,
+			email: userData.email,
+			password: userData.password ? md5(userData.password) : undefined,
+			isAvatarImageSet: userData.isAvatarImageSet,
+			avatarImage: userData.avatarImage,
+			updateDate: moment().format("YYYY-MM-DD HH:mm:ss"),
+		};
+
+		// Eliminamos los campos que no se van a mofificar
+		userObj = await removeUndefinedKeys(userObj);
+
+		return await db.query(
+			"UPDATE users SET ? WHERE id = ?",
+			[userObj, id],
+			"update",
+			conn
+		);
+	} catch (e) {
+		throw new Error(e);
+	} finally {
+		conn && (await conn.end());
+	}
+};
+
 module.exports = userDao;
