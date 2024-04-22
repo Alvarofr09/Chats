64920d81f37365f16dc0ceb010e2ca9b338d5250
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-04-22 12:32:08 +0200

    cambiar nombre bbdd

--- server/services/db.js
+++ server/services/db.js
@@ -10,7 +10,7 @@ db.createConection = async () => {
 			port: process.env.DB_PORT || 3306,
 			user: process.env.DB_USER || "root",
 			password: process.env.DB_PASSWORD || "1234",
-			database: process.env.DB_NAME || "test",
+			database: process.env.DB_NAME || "test2",
 			dateStrings: true,
 		});
 		console.log("Database connected, TRUE");
