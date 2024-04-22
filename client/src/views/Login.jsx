5ddf787eb4c8dec5a94b522d91cc146ae26b9410
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-22 20:21:03 +0200

    Vista de login

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,3 +1,9 @@
+import LoginForm from "../components/LoginForm/LoginForm";
+
 export default function Login() {
-	return <div>Login</div>;
+	return (
+		<div className="bg-black h-screen flex justify-center items-center">
+			<LoginForm />
+		</div>
+	);
 }
