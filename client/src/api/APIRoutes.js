3cb0fb95c16e8bf6c7ff1109c04aeb9592b0f38b
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-22 23:11:10 +0200

    ruta de traer los contactos

--- client/src/api/APIRoutes.js
+++ client/src/api/APIRoutes.js
@@ -5,6 +5,7 @@ const host = "http://localhost:3000";
 export const registerRoute = `/api/auth/register`;
 export const loginRoute = `/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const getAllUsersRoute = `${host}/api/auth/allUsers`;
 
 export const userApi = axios.create({
 	baseURL: `${host}`,
