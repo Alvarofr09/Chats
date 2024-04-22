3fcca1374c120c47dac85ca22d9ef7a2e4fd135b
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-22 20:19:27 +0200

    Contexto y setAvatar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./views/Register";
 import Chat from "./views/Chat";
 import Login from "./views/Login";
+import AuthContextProvider from "./context/AuthContext";
+import SetAvatar from "./views/SetAvatar";
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/register" element={<Register />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/" element={<Chat />} />
-			</Routes>
+			<AuthContextProvider>
+				<Routes>
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/setAvatar" element={<SetAvatar />} />
+					<Route path="/" element={<Chat />} />
+				</Routes>
+			</AuthContextProvider>
 		</BrowserRouter>
 	);
 }
