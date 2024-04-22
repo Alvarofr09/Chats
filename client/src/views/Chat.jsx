530b0568444902565fd08c418846123aa5c98365
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-22 20:20:51 +0200

    hacer bootn de logout para pruebas

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -1,3 +1,11 @@
+import { useAuthContext } from "../context/AuthContext";
+
 export default function Chat() {
-	return <div>Chat</div>;
+	const { logout } = useAuthContext();
+	return (
+		<div>
+			<h1>Chat</h1>
+			<button onClick={logout}>Logout</button>
+		</div>
+	);
 }
