8a08c912dd47fc9c73113320303ecdb6be9f073d
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-04-22 12:30:52 +0200

    cambiar cosas del input bonito

--- client/src/components/ui/Prueba.jsx
+++ client/src/components/ui/Prueba.jsx
@@ -9,13 +9,11 @@ export default function InputPrueba({ label, ...props }) {
 					{...props}
 					{...field}
 					className={
-						meta.touched && meta.error
-							? "input-error"
-							: "h-20 w-96 px-6 text-4xl bg-black border-2 rounded-lg border-white border-opacity-50 outline-none focus:border-sky-500 focus:text-white transition duration-200 "
+						meta.touched && meta.error ? "input-error" : "register-input"
 					}
 				/>
 				<span
-					className="text-4xl text-white text-opacity-80 absolute left-0 top-4
+					className="text-3xl text-white text-opacity-80 absolute left-0 top-4
                 mx-6 px-2 transition duration-200
                 input-text"
 				>
