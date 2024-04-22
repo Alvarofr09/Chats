d53e7ebb5141174fe3d608e888a4c6c9e41841a7
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-22 20:22:43 +0200

    controlador de login y de setAvatar

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const dao = require("../services/dao/userDao");
+const md5 = require("md5");
+const path = require("path");
 
 const userRegister = async (req, res, next) => {
 	try {
@@ -33,6 +35,67 @@ const userRegister = async (req, res, next) => {
 	}
 };
 
+const userLogin = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
+		console.log(req.body);
+
+		let user = await dao.getUserByEmail(email);
+		if (user.length <= 0) return res.status(404).send("Usuario no registrado");
+
+		const clientPassword = md5(password);
+		[user] = user;
+
+		if (user.password !== clientPassword) return res.sendStatus(401);
+
+		delete user.password;
+
+		res.status(201).json({ user, status: true });
+	} catch (error) {
+		next(error);
+	}
+};
+
+const setAvatar = async (req, res, next) => {
+	try {
+		const userId = req.params.id;
+		const { image } = req.body;
+		// let userData = await dao.getUserByEmail(userId);
+		// [userData] = userData;
+
+		if (image) {
+			const images = !image.length ? [image] : image;
+
+			const user = await dao.setAvatar(userId, image);
+
+			if (!user) return res.json({ status: false });
+
+			const userObj = {
+				isAvatarImageSet: true,
+				avatarImage: image,
+			};
+
+			const updatedUser = await dao.updateUser(userId, userObj);
+
+			if (!updatedUser) return res.json({ status: false });
+
+			let userData = await dao.getUserById(userId);
+			[userData] = userData;
+
+			return res.json({
+				isSet: userData.isAvatarImageSet,
+				image: userData.avatarImage,
+			});
+		}
+
+		// if (user.isAvatarImageSet) return res.json({ isSet: true });
+	} catch (error) {
+		next(error);
+	}
+};
+
 module.exports = {
 	userRegister,
+	userLogin,
+	setAvatar,
 };
