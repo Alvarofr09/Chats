e17dc7107dc6d14051fcf5c23254a0dc7defc795
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-23 10:49:54 +0200

    ruta de traer usuarios

--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -3,6 +3,7 @@ const {
 	userRegister,
 	userLogin,
 	setAvatar,
+	getAllUsers,
 } = require("../controllers/userController");
 
 const userRouter = express.Router();
@@ -11,6 +12,6 @@ userRouter.post("/register", userRegister);
 userRouter.post("/login", userLogin);
 userRouter.post("/setAvatar/:id", setAvatar);
 
-userRouter.get("/allUsers/:id");
+userRouter.get("/allUsers/:id", getAllUsers);
 
 module.exports = userRouter;
