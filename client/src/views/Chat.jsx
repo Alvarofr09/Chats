a09ec1ce53c29d16b173231fbe007c702d5be4e4
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-23 13:49:44 +0200

    refactorizar un poco

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -1,53 +1,63 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
 import axios from "axios";
-
 import { getAllUsersRoute } from "../api/APIRoutes";
-
 import Contacts from "../components/Contacts";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Chat() {
 	const navigate = useNavigate();
-	const [contacs, setContacs] = useState([]);
+	const [contacts, setContacts] = useState([]);
 	const [currentUser, setCurrentUser] = useState(undefined);
 
 	useEffect(() => {
-		const getCurrentUser = async () => {
+		const fetchData = async () => {
 			if (!localStorage.getItem("user")) {
 				navigate("/login");
-			} else {
-				setCurrentUser(await JSON.parse(localStorage.getItem("user")));
+				return;
+			}
+
+			const user = JSON.parse(localStorage.getItem("user"));
+			setCurrentUser(user);
+
+			if (!user.isAvatarImageSet) {
+				navigate("/setAvatar");
+				return;
+			}
+
+			try {
+				const response = await axios.get(`${getAllUsersRoute}/${user.id}`);
+				setContacts(response.data.users);
+			} catch (error) {
+				console.error("Error fetching contacts:", error);
+				toast.error("Error fetching contacts. Please try again.", {
+					position: "bottom-right",
+					autoClose: 5000,
+					pauseOnHover: true,
+					draggable: true,
+					theme: "dark",
+				});
 			}
 		};
 
-		getCurrentUser();
-	}, []);
-	useEffect(() => {
-		const getAllUsers = async () => {
-			if (currentUser) {
-				if (currentUser.isAvatarImageSet) {
-					const data = await axios.get(`${getAllUsersRoute}/${currentUser.id}`);
-					setContacs(data.data);
-				} else {
-					navigate("/setAvatar");
-				}
-			}
-		};
+		fetchData();
+	}, [navigate]);
 
-		getAllUsers();
-	}, [currentUser]);
 	return (
-		<div
-			className="h-screen w-screen flex flex-col justify-center gap-4
-		items-center bg-[#131324] "
-		>
+		<div className="h-screen w-screen flex flex-col justify-center gap-4 items-center bg-[#131324]">
 			<div
-				className="container h-[85%] w-[85%] bg-[#00000076]
-			grid grid-cols-[25% 75%] lg:grid-cols-[35% 65%]"
+				className="container h-[85%] w-[85%] bg-[#00000076] grid grid-cols-2 lg:grid-cols-[35% 65%]"
+				style={{
+					gridTemplateColumns: "25% 75%",
+					"@media (min-width: 720px) and (max-width: 1080px)": {
+						gridTemplateColumns: "35% 65%",
+					},
+				}}
 			>
-				<Contacts contacs={contacs} currentUser={currentUser} />
+				<Contacts contacts={contacts} currentUser={currentUser} />
 			</div>
+			<ToastContainer />
 		</div>
 	);
 }
