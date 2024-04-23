0cb8c27f1399662cddfaea1fbf38898a1c72c588
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-23 13:49:58 +0200

    Traer los contactos

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -17,24 +17,123 @@ export default function Contacts({ contacts, currentUser }) {
 	return (
 		<>
 			{currentUserImage && currentUserName && (
-				<div className="container">
-					<div className="brand">
-						<img src={Logo} alt="Logo" />
-						<h1>Tradeium</h1>
+				<div
+					className="container grid grid-rows-3 overflow-hidden bg-[#080420]"
+					style={{ gridTemplateRows: "10% 75% auto" }}
+				>
+					<div className="brand flex justify-center items-center gap-4">
+						<img src={Logo} alt="Logo" className="h-8" />
+						<h1 className="titulo uppercase">Tradeium</h1>
 					</div>
-					<div className="contacts">
+					<div className="contacts flex flex-col justify-center items-center overflow-auto gap-3 scrollbar-custom">
 						{contacts.map((contact, index) => {
 							return (
 								<div
-									className={`contact ${
-										index === currentUserSelected && "selected"
+									className={`contact bg-[#ffffff39] min-h-[5rem] w-[90%] cursor-pointer rounded-[0.2rem] 
+									gap-4 p-[0.4rem] items-center flex transition duration-200 ease-in-out ${
+										index === currentUserSelected && "bg-[#9186f3]"
 									}`}
 									key={index}
 								>
-									<div className="avatar"></div>
+									<div className="avatar">
+										<img
+											className="h-12"
+											src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+											alt="avatar"
+										/>
+									</div>
+									<div className="username">
+										<h3 className="text-xl text-white bold">
+											{contact.username}
+										</h3>
+									</div>
 								</div>
 							);
 						})}
+						{contacts.map((contact, index) => {
+							return (
+								<div
+									className={`contact bg-[#ffffff39] min-h-[5rem] w-[90%] cursor-pointer rounded-[0.2rem] 
+									gap-4 p-[0.4rem] items-center flex transition duration-200 ease-in-out ${
+										index === currentUserSelected && "bg-[#9186f3]"
+									}`}
+									key={index}
+								>
+									<div className="avatar">
+										<img
+											className="h-12"
+											src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+											alt="avatar"
+										/>
+									</div>
+									<div className="username">
+										<h3 className="text-xl text-white bold">
+											{contact.username}
+										</h3>
+									</div>
+								</div>
+							);
+						})}
+						{contacts.map((contact, index) => {
+							return (
+								<div
+									className={`contact bg-[#ffffff39] min-h-[5rem] w-[90%] cursor-pointer rounded-[0.2rem] 
+									gap-4 p-[0.4rem] items-center flex transition duration-200 ease-in-out ${
+										index === currentUserSelected && "bg-[#9186f3]"
+									}`}
+									key={index}
+								>
+									<div className="avatar">
+										<img
+											className="h-12"
+											src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+											alt="avatar"
+										/>
+									</div>
+									<div className="username">
+										<h3 className="text-xl text-white bold">
+											{contact.username}
+										</h3>
+									</div>
+								</div>
+							);
+						})}
+						{contacts.map((contact, index) => {
+							return (
+								<div
+									className={`contact bg-[#ffffff39] min-h-[5rem] w-[90%] cursor-pointer rounded-[0.2rem] 
+									gap-4 p-[0.4rem] items-center flex transition duration-200 ease-in-out ${
+										index === currentUserSelected && "bg-[#9186f3]"
+									}`}
+									key={index}
+								>
+									<div className="avatar">
+										<img
+											className="h-12"
+											src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+											alt="avatar"
+										/>
+									</div>
+									<div className="username">
+										<h3 className="text-xl text-white bold">
+											{contact.username}
+										</h3>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+					<div className="current-user bg-[#0d0d30] flex justify-center items-center gap-8">
+						<div className="avatar h-16">
+							<img
+								className="h-full max-i"
+								src={`data:image/svg+xml;base64,${currentUserImage}`}
+								alt="avatar"
+							/>
+						</div>
+						<div className="username">
+							<h3 className="text-xl text-white bold">{currentUserName}</h3>
+						</div>
 					</div>
 				</div>
 			)}
