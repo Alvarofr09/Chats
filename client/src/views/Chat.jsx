463d33fd14145f91b495a64a1a174b03058cb26f
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-25 18:13:30 +0200

    Pasar el usuario

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -81,7 +81,7 @@ export default function Chat() {
 				{isLoaded && currentChat === undefined ? (
 					<Welcome currentUser={currentUser} />
 				) : (
-					<ChatContainer currentChat={currentChat} />
+					<ChatContainer currentChat={currentChat} currentUser={currentUser} />
 				)}
 			</div>
 			<ToastContainer />
