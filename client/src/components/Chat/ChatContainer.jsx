a8befad6f5159c281605f448be6efa1f3cf7c079
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-04-26 11:01:20 +0200

    useEffect de traer los datos, css del contenedor del chat y css de los mensajes en si

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -1,11 +1,26 @@
 import ChatInput from "./ChatInput";
 import Logout from "../Logout";
-import Messages from "./Messages";
 
 import axios from "axios";
-import { sendMessageRoute } from "../../api/APIRoutes";
+import { getAllMessages, sendMessageRoute } from "../../api/APIRoutes";
+import { useEffect, useState } from "react";
 
 export default function ChatContainer({ currentChat, currentUser }) {
+	const [messages, setMessages] = useState([]);
+	useEffect(() => {
+		async function fetchData() {
+			const response = await axios.post(getAllMessages, {
+				from: currentUser.id,
+				to: currentChat.id,
+			});
+
+			setMessages(response.data);
+		}
+
+		fetchData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentChat]);
+
 	const handleSendMsg = async (msg) => {
 		try {
 			await axios.post(sendMessageRoute, {
@@ -20,7 +35,10 @@ export default function ChatContainer({ currentChat, currentUser }) {
 	return (
 		<>
 			{currentChat && (
-				<div className="container pt-4">
+				<div
+					className="container pt-4 grid grid-rows-3 gap-1 overflow-hidden"
+					style={{ gridTemplateRows: "10% 78% 12%" }}
+				>
 					<div className="chat-header flex justify-between items-center px-8 py-0">
 						<div className="user-details flex items-center gap-4">
 							<div className="user-image">
@@ -38,7 +56,23 @@ export default function ChatContainer({ currentChat, currentUser }) {
 						</div>
 						<Logout />
 					</div>
-					<Messages />
+					<div className="chat-messages py-4 px-8 flex flex-col gap-4 overflow-auto">
+						{messages.map((message, index) => {
+							return (
+								<div key={index}>
+									<div
+										className={`message  flex items-center ${
+											message.fromSelf ? "sended" : "recieved"
+										}`}
+									>
+										<div className="content max-w-[40%] overflow-wrap-break-word p-4 text-lg rounded-2xl text-[#d1d1d1]">
+											{message.message}
+										</div>
+									</div>
+								</div>
+							);
+						})}
+					</div>
 					<ChatInput handleSendMsg={handleSendMsg} />
 				</div>
 			)}
