6ae9e046ff61e8bea1d5d1ed5a4e8393dea6d104
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-04-27 11:25:53 +0200

    controladores de crearGrupos, añadir a grupos y traer todos los grupos

---
+++ server/controllers/groupsController.js
@@ -0,0 +1,64 @@
+const dao = require("../services/dao/groupsDao");
+
+const addGroup = async (req, res, next) => {
+	try {
+		const { group_name, description } = req.body;
+
+		const groupData = {
+			group_name,
+			description,
+		};
+
+		const data = await dao.createGroup(groupData);
+
+		if (!data)
+			return res
+				.status(500)
+				.json({ message: "Error al enviar el mensaje", status: false });
+
+		return res.json({ message: "Grupo creado correctamente", status: true });
+	} catch (error) {
+		next(error);
+	}
+};
+
+const addToGroup = async (req, res, next) => {
+	try {
+		const { group_id, user_id } = req.body;
+
+		const membershipData = {
+			group_id,
+			user_id,
+		};
+
+		const data = await dao.addToGroup(membershipData);
+
+		if (!data)
+			return res
+				.status(500)
+				.json({ message: "Error al enviar el mensaje", status: false });
+
+		return res.json({ message: "Grupo creado correctamente", status: true });
+	} catch (error) {
+		next(error);
+	}
+};
+
+const getAllGroups = async (req, res, next) => {
+	try {
+		const { user_id } = req.body;
+
+		const groups = await dao.getAllGroups(user_id);
+
+		if (groups.length === 0)
+			return res
+				.status(500)
+				.json({ message: "No estas en ningun grupo", status: false });
+
+		return res.json(groups);
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = { addGroup, addToGroup, getAllGroups };
