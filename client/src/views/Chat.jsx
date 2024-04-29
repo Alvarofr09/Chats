03e76b10dfffbfd5a0398fad9b022b24204059da
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-29 11:49:57 +0200

    comentario

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -19,6 +19,8 @@ export default function Chat() {
 	const [currentChat, setCurrentChat] = useState(undefined);
 	const [isLoaded, setIsLoaded] = useState(false);
 
+	// const token = JSON.parse(localStorage.getItem("token"));
+
 	useEffect(() => {
 		const fetchData = async () => {
 			if (!localStorage.getItem("token")) {
