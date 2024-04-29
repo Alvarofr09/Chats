9aba881b7f04a2c211083ded3888d0cbbb2190b6
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-29 10:14:51 +0200

    cambiar propiedades y algunos nombres de tablas

--- server/utils/dbTables.js
+++ server/utils/dbTables.js
@@ -1,100 +1,85 @@
+const db = require("../services/db");
+
 const createUsersTable = async () => {
-	let conn = null;
+	let conn = await db.createConection();
 	try {
-		conn = await db.createConection();
-
-		let SqlQuery = `
-		CREATE TABLE IF NOT EXISTS users (
-			id INT AUTO_INCREMENT, 
-			username VARCHAR(255), 
-			email VARCHAR(255), 
-			password VARCHAR(255),
-			userRole BOOLEAN DEFAULT FALSE,
-			superAdmin BOOLEAN DEFAULT FALSE,
-			isAvatarImageSet BOOLEAN DEFAULT FALSE,
-			avatarImage longtext,
-			registerDate datetime,
-			updateDate datetime,
-			PRIMARY KEY (id)
-		) `;
+		const SqlQuery = `
+      CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY, 
+        username VARCHAR(255), 
+        email VARCHAR(255), 
+        password VARCHAR(255),
+        userRole BOOLEAN DEFAULT FALSE,
+        superAdmin BOOLEAN DEFAULT FALSE,
+        isAvatarImageSet BOOLEAN DEFAULT FALSE,
+        avatarImage TEXT,
+        registerDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      ) `;
 		await db.query(SqlQuery, null, "create", conn);
-	} catch (error) {
-		throw new Error(error);
 	} finally {
-		conn && (await conn.end());
-	}
-};
-
-const createMessagesTable = async () => {
-	let conn = null;
-	try {
-		conn = await db.createConection();
-
-		let SqlQuery = `
-		CREATE TABLE IF NOT EXISTS messages (
-    		id INT AUTO_INCREMENT, 
-    		sender_id INT, 
-    		receiver_id INT,
-			group_id INT,
-    		text LONGTEXT,
-    		date DATETIME, 
-    		PRIMARY KEY (id),
-    		FOREIGN KEY (sender_id) REFERENCES users(id),
-    		FOREIGN KEY (receiver_id) REFERENCES users(id),
-			FOREIGN KEY (group_id) REFERENCES groups(id)
-		) `;
-		await db.query(SqlQuery, null, "create", conn);
-	} catch (error) {
-		throw new Error(error);
-	} finally {
-		conn && (await conn.end());
+		await conn.end();
 	}
 };
 
 const createGroupsTable = async () => {
-	let conn = null;
+	let conn = await db.createConection();
 	try {
-		conn = await db.createConection();
-
-		let SqlQuery = `
-		CREATE TABLE IF NOT EXISTS groups (
-    		id INT AUTO_INCREMENT PRIMARY KEY,
-    		group_name VARCHAR(255) NOT NULL,
-    		description TEXT,
-    		creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		); `;
+		const SqlQuery = `
+        CREATE TABLE IF NOT EXISTS grupos (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            group_name VARCHAR(255) ,
+            description TEXT,
+            creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ); `;
 		await db.query(SqlQuery, null, "create", conn);
-	} catch (error) {
-		throw new Error(error);
 	} finally {
-		conn && (await conn.end());
+		await conn.end();
+	}
+};
+
+const createMessagesTable = async () => {
+	let conn = await db.createConection();
+	try {
+		const SqlQuery = `
+      CREATE TABLE IF NOT EXISTS messages (
+        id INT AUTO_INCREMENT,
+        sender_id INT,
+        receiver_id INT,
+        group_id INT,
+        text TEXT,
+        date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id),
+        FOREIGN KEY (sender_id) REFERENCES users(id),
+        FOREIGN KEY (receiver_id) REFERENCES users(id),
+        FOREIGN KEY (group_id) REFERENCES grupos(id)
+      ) `;
+		await db.query(SqlQuery, null, "create", conn);
+	} finally {
+		await conn.end();
 	}
 };
 
 const createMembershipTable = async () => {
-	let conn = null;
+	let conn = await db.createConection();
 	try {
-		conn = await db.createConection();
-
-		let SqlQuery = `
-		CREATE TABLE IF NOT EXISTS group_membership (
-    		id INT AUTO_INCREMENT PRIMARY KEY,
-    		group_id INT,
-    		user_id INT,
-    		FOREIGN KEY (id_group) REFERENCES groups(id),
-    		FOREIGN KEY (id_user) REFERENCES users(id)
-		); `;
+		const SqlQuery = `
+      CREATE TABLE IF NOT EXISTS grupos_membership (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        group_id INT,
+        user_id INT,
+        FOREIGN KEY (group_id) REFERENCES grupos(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+      ); `;
 		await db.query(SqlQuery, null, "create", conn);
-	} catch (error) {
-		throw new Error(error);
 	} finally {
-		conn && (await conn.end());
+		await conn.end();
 	}
 };
 
 module.exports = {
 	createUsersTable,
-	createMessagesTable,
 	createGroupsTable,
+	createMessagesTable,
 	createMembershipTable,
 };
