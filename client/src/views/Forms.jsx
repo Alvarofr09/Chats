f12e0448fed5760925687442327f731d08db4612
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-04-29 17:55:22 +0200

    Ruta Forms

---
+++ client/src/views/Forms.jsx
@@ -0,0 +1,12 @@
+import ButtonGroup from "../components/ui/ButtonGroup";
+
+import LoginForm from "../components/LoginForm/LoginForm";
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+export default function Forms() {
+	return (
+		<div className="bg-gradient-to-br from-blue-400 to-black h-screen w-screen">
+			<ButtonGroup />
+		</div>
+	);
+}
