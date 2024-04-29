7da8f8e9716af856a8c2ed81e2a067815363b7d9
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-29 14:02:52 +0200

    cambiar css

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/LoginForm/LoginForm";
 
 export default function Login() {
 	return (
-		<div className="bg-black h-screen centered">
+		<div className="bg-gradient-to-br from-blue-400 to-black h-screen w-screen">
 			<LoginForm />
 		</div>
 	);
