72a96c847c0fe2570d6852ef46792c928e5e7a16
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-30 13:35:42 +0200

    hacer los botones bien

--- client/src/components/ui/ButtonGroup.jsx
+++ client/src/components/ui/ButtonGroup.jsx
@@ -1,34 +1,39 @@
 import { useState } from "react";
 
-export default function ButtonGroup() {
-	const [activeButton, setActiveButton] = useState("login");
+import LoginForm from "../LoginForm/LoginForm";
+import RegisterForm from "../RegisterForm/RegisterForm";
 
-	const handleButtonClick = (buttonName) => {
-		setActiveButton(buttonName);
-	};
+export default function ButtonGroup() {
+	const [view, setView] = useState("login");
 
 	return (
-		<div className="flex gap-4 rounded-3xl bg-[#2e2d2d] ">
-			<button
-				onClick={() => handleButtonClick("login")}
-				className={`px-4 rounded-3xl py-2 transition ease-in-out duration-300 ${
-					activeButton === "login"
-						? "bg-white text-[#2e2d2d]"
-						: "bg-transparent text-white"
-				}`}
-			>
-				Login
-			</button>
-			<button
-				onClick={() => handleButtonClick("register")}
-				className={`px-4 rounded-3xl py-2 transition ease-in-out duration-300 ${
-					activeButton === "register"
-						? "bg-white text-[#2e2d2d]"
-						: "bg-transparent text-white"
-				}`}
-			>
-				Register
-			</button>
+		<div className="h-screen flex flex-col items-center justify-evenly">
+			<div className="brand mt-12">
+				<h1 className="titulo">Trademiun</h1>
+			</div>
+			{view === "login" ? <LoginForm /> : <RegisterForm />}
+			<div className="flex flex-row gap-4 mb-12 rounded-3xl bg-[#2e2d2d] ">
+				<button
+					onClick={() => setView("login")}
+					className={`px-4 rounded-3xl py-2 transition ease-in-out duration-300 ${
+						view === "login"
+							? "bg-white text-[#2e2d2d]"
+							: "bg-transparent text-white"
+					}`}
+				>
+					Login
+				</button>
+				<button
+					onClick={() => setView("register")}
+					className={`px-4 rounded-3xl py-2 transition ease-in-out duration-300 ${
+						view === "register"
+							? "bg-white text-[#2e2d2d]"
+							: "bg-transparent text-white"
+					}`}
+				>
+					Register
+				</button>
+			</div>
 		</div>
 	);
 }
