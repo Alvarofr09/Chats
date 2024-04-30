a229e66cdb4567d55ea7b3fc471e8ba3132588cb
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-30 13:36:13 +0200

    clase css y quitar import

--- client/src/views/Forms.jsx
+++ client/src/views/Forms.jsx
@@ -1,11 +1,8 @@
 import ButtonGroup from "../components/ui/ButtonGroup";
 
-import LoginForm from "../components/LoginForm/LoginForm";
-import RegisterForm from "../components/RegisterForm/RegisterForm";
-
 export default function Forms() {
 	return (
-		<div className="bg-gradient-to-br from-blue-400 to-black h-screen w-screen">
+		<div className="background h-screen w-screen">
 			<ButtonGroup />
 		</div>
 	);
