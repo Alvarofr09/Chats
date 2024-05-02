3f04ceb0eea721e4a19e35ead9c4ae899ebf3ec0
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-02 12:20:50 +0200

    Rutas de prueba

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Login from "./views/Login";
 import AuthContextProvider from "./context/AuthContext";
 // import SetAvatar from "./views/SetAvatar";
 import Forms from "./views/Forms";
+import UserDetails from "./views/UserDetails";
+import CreateGroup from "./views/CreateGroup";
 
 export default function App() {
 	return (
@@ -12,6 +14,8 @@ export default function App() {
 			<AuthContextProvider>
 				<Routes>
 					<Route path="/register" element={<Register />} />
+					<Route path="/user/:id" element={<UserDetails />} />
+					<Route path="/create-group/:id" element={<CreateGroup />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/forms" element={<Forms />} />
 					<Route path="/" element={<Chat />} />
