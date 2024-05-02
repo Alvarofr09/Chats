920652a995c59ffaf024781b493b9d7292c89cc6
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-02 12:20:25 +0200

    Ruta de traer datos del usuario

--- client/src/api/APIRoutes.js
+++ client/src/api/APIRoutes.js
@@ -6,6 +6,7 @@ export const registerRoute = `/api/auth/register`;
 export const loginRoute = `/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const getAllUsersRoute = `${host}/api/auth/allUsers`;
+export const getUserInfo = `${host}/api/auth/getUserInfo`;
 
 export const sendMessageRoute = `${host}/api/messages/add-message`;
 export const getAllMessages = `${host}/api/messages/getMessages`;
