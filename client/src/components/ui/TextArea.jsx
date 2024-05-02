27c938d75186320ed794baf67554fe9edf06d378
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-02 12:22:07 +0200

    Textarea (fallido)

---
+++ client/src/components/ui/TextArea.jsx
@@ -0,0 +1,20 @@
+import { useField } from "formik";
+
+export default function TextArea({ props }) {
+	const [field, meta] = useField(props);
+	return (
+		<div className="input-container">
+			<textarea
+				{...props}
+				{...field}
+				className={
+					meta.touched && meta.error ? "input-error" : "register-input"
+				}
+				cols="4"
+				rows="40"
+			>
+				Hola
+			</textarea>
+		</div>
+	);
+}
