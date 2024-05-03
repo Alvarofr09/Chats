20450ace403bd3ac4195569d5c87ba83fec6e2e1
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-03 11:53:28 +0200

    cambiar la ruta de la imagenes y asignar clases

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -2,12 +2,8 @@ export default function ChatHeader({ currentChat }) {
 	return (
 		<div className="chat-header flex justify-between items-center px-8 py-0">
 			<div className="user-details flex items-center gap-4">
-				<div className="user-image">
-					<img
-						className="h-12"
-						src={`data:image/svg+xml;base64,${currentChat.image}`}
-						alt="avatar"
-					/>
+				<div className="user-image avatar-image">
+					<img className="h-12" src={currentChat.image} alt="avatar" />
 				</div>
 				<div className="username">
 					<h3 className="text-white text-xl font-bold">
