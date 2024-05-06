017907cb5f10c8b3799ac553b2a6a257e9def157
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-06 09:26:13 +0200

    Ruta nueva

--- server/routers/messagesRoutes.js
+++ server/routers/messagesRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
 	addMessage,
 	getAllMessages,
+	getAllGroupMessages,
 } = require("../controllers/messagesController");
 
 const messageRouter = express.Router();
@@ -9,5 +10,6 @@ const messageRouter = express.Router();
 messageRouter.post("/add-message", addMessage);
 
 messageRouter.post("/getMessages", getAllMessages);
+messageRouter.post("/getGroupMessages", getAllGroupMessages);
 
 module.exports = messageRouter;
