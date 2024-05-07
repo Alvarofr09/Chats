3dea70f529c9f818e03777bb191e45e88942ff69
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-07 13:14:28 +0200

    Que te muestre los participantes

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -1,6 +1,7 @@
 export default function ChatHeader({ currentChat }) {
 	return (
-		<div className="chat-header flex justify-between items-center px-8 py-0">
+		<div className="chat-header bg-[#2B2A29] flex justify-between items-center px-8 py-0">
+			{console.log(currentChat)}
 			<div className="user-details flex items-center gap-4">
 				<div className="user-image avatar-image">
 					<img className="h-12" src={currentChat.image} alt="avatar" />
@@ -9,6 +10,9 @@ export default function ChatHeader({ currentChat }) {
 					<h3 className="text-white text-xl font-bold">
 						{currentChat.group_name}
 					</h3>
+					<span className="text-[#9C9999]">
+						{currentChat.participantes} Participantes
+					</span>
 				</div>
 			</div>
 		</div>
