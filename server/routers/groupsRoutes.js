5746315bd7c532f0d59f6ab7f101b7ff3faadbcf
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-07 11:42:54 +0200

    ruta

--- server/routers/groupsRoutes.js
+++ server/routers/groupsRoutes.js
@@ -3,12 +3,14 @@ const {
 	createGroup,
 	joinGroup,
 	getAllGroups,
+	isAdmin,
 } = require("../controllers/groupsController");
 
 const groupRouter = express.Router();
 
 groupRouter.post("/create-group", createGroup);
 groupRouter.post("/join-group", joinGroup);
+groupRouter.post("/is-admin/:id", isAdmin);
 
 groupRouter.get("/get-groups/:id", getAllGroups);
 
