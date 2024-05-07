a0eb9f9b612fd7c0f5d5ba9ac454240c17fcc3fb
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-07 20:11:16 +0200

    Hacer componente select

---
+++ client/src/components/ui/Select.jsx
@@ -0,0 +1,19 @@
+import { useField } from "formik";
+
+export default function Select({ label, children, ...props }) {
+	const [field, meta] = useField(props);
+
+	return (
+		<>
+			<label htmlFor="">{label}</label>
+			<select
+				{...props}
+				{...field}
+				className={meta.touched && meta.error ? "input-error" : "input"}
+			>
+				{children}
+			</select>
+			{meta.touched && meta.error && <div className="error">{meta.error}</div>}
+		</>
+	);
+}
