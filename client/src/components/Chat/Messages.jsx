a4236417672a0f5477a620965502b325f963422b
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-07 13:15:16 +0200

    Darle mas width

--- client/src/components/Chat/Messages.jsx
+++ client/src/components/Chat/Messages.jsx
@@ -4,7 +4,6 @@ export default function Messages({ messages, scrollRef }) {
 	return (
 		<div className="chat-messages scrollbar-custom py-4 px-8 flex flex-col gap-4 overflow-auto">
 			{messages.map((message) => {
-				console.log(message);
 				return (
 					<div ref={scrollRef} key={uuidv4()}>
 						<div
@@ -12,7 +11,7 @@ export default function Messages({ messages, scrollRef }) {
 								message.fromSelf ? "sended" : "recieved"
 							}`}
 						>
-							<div className="content max-w-[40%] overflow-wrap-break-word p-4 text-lg rounded-2xl text-[#d1d1d1]">
+							<div className="content max-w-[60%] break-all p-4 text-lg rounded-2xl text-[#000000]">
 								{message.message}
 							</div>
 						</div>
