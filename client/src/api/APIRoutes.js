0bb6ffd8e139512795d4c0e0fd69b2512cada54c
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-05-07 11:42:08 +0200

    Ruta api

--- client/src/api/APIRoutes.js
+++ client/src/api/APIRoutes.js
@@ -15,6 +15,7 @@ export const getAllGroupMessages = `${host}/api/messages/getGroupMessages`;
 export const createGroupRoute = `${host}/api/groups/create-group`;
 export const joinGroupRoute = `${host}/api/groups/join-group`;
 export const getAllGroups = `${host}/api/groups/get-groups`;
+export const isAdmin = `${host}/api/groups/is-admin`;
 
 export const userApi = axios.create({
 	baseURL: `${host}`,
