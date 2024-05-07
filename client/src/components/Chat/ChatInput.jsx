6e78b4b4b2cee5ba38bab31886d2115273c9e5b8
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-05-07 20:10:31 +0200

    Mostrar modal

--- client/src/components/Chat/ChatInput.jsx
+++ client/src/components/Chat/ChatInput.jsx
@@ -1,17 +1,16 @@
 import Picker from "emoji-picker-react";
 import { IconSend2 } from "@tabler/icons-react";
 import { IconMoodSmileFilled } from "@tabler/icons-react";
+import { IconClipboardData } from "@tabler/icons-react";
+import { IconChevronLeft } from "@tabler/icons-react";
 import { useState } from "react";
+import Modal from "../Modal";
+import SignalForm from "../SignalForm/SignalForm";
 
-export default function ChatInput({ handleSendMsg }) {
+export default function ChatInput({ handleSendMsg, isAdmin }) {
 	const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-	const [isSignal, setIsSignal] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 	const [message, setMessage] = useState("");
-	const [signal, setSignal] = useState({
-		coin: "",
-		buyPrice: "",
-		sellPrice: "",
-	});
 
 	const handleEmojiPickerHideShow = () => {
 		setShowEmojiPicker(!showEmojiPicker);
@@ -31,93 +30,104 @@ export default function ChatInput({ handleSendMsg }) {
 		}
 	};
 
-	const handleSignalChange = (event) => {
-		setIsSignal(event.target.checked);
+	const showSignalForm = () => {
+		setShowModal(true); // Mostrar el modal al activar la función
+	};
+
+	const closeModal = () => {
+		setShowModal(false); // Cerrar el modal
 	};
 
 	return (
-		<div
-			className="container  grid grid-cols-2 items-center bg-[#000000] px-8 py-0 pb-[0.3rem]"
-			style={{
-				gridTemplateColumns: "5% 95%",
-			}}
-		>
-			<div className="button-container flex items-center text-white gap-4">
-				<div className="emoji relative">
-					{showEmojiPicker && (
-						<Picker className="" onEmojiClick={handleEmojiClick} />
+		<div className="container gap-6  centered bg-[#000000] px-8 py-0 pb-[0.3rem]">
+			{isAdmin ? (
+				<>
+					<div className="button-container flex items-center text-white gap-4">
+						<div className="emoji relative">
+							{showEmojiPicker && (
+								<Picker className="" onEmojiClick={handleEmojiClick} />
+							)}
+							<IconMoodSmileFilled
+								onClick={handleEmojiPickerHideShow}
+								color="#ffff00c8"
+								className="cursor-pointer"
+								size={30}
+							/>
+						</div>
+					</div>
+
+					<form
+						onSubmit={(e) => sendChat(e)}
+						className="input-container  w-[100%] rounded-[2rem] flex content-center gap-8 bg-[#D9D9D9]"
+					>
+						<>
+							<input
+								type="text"
+								value={message}
+								onChange={(e) => setMessage(e.target.value)}
+								placeholder="Type your message here"
+								className="w-[90%]  bg-transparent border-none pl-4 text-xl text-[#585858] focus:outline-none"
+							/>
+							<button
+								type="submit"
+								className="submit-btn md:py-0 px-8 lg:py-1 rounded-[2rem] centered  border-none"
+							>
+								<IconSend2 color="#585858" size={32} />
+							</button>
+						</>
+					</form>
+
+					<div className="signal" onClick={showSignalForm}>
+						<IconClipboardData color="#ffffff" />
+					</div>
+
+					{showModal && (
+						<Modal closeModal={closeModal}>
+							<SignalForm />
+						</Modal>
 					)}
-					<IconMoodSmileFilled
-						onClick={handleEmojiPickerHideShow}
-						color="#ffff00c8"
-						className="cursor-pointer"
-						size={30}
-					/>
+				</>
+			) : (
+				<div className="centered gap-4">
+					<button className="btn-dark">Silenciar</button>
+					<button className="btn-rounded p-2">
+						<IconChevronLeft color="#000000" size={20} />
+					</button>
 				</div>
-			</div>
-
-			<form
-				onSubmit={(e) => sendChat(e)}
-				className="input-container  w-[100%] rounded-[2rem] flex content-center gap-8 bg-[#D9D9D9]"
-			>
-				<input
-					type="checkbox"
-					checked={isSignal}
-					onChange={handleSignalChange}
-					className="mr-2"
-				/>
-				<label htmlFor="isSignal" className="text-white">
-					Signal
-				</label>
-				{!isSignal ? (
-					<>
-						<input
-							type="text"
-							value={message}
-							onChange={(e) => setMessage(e.target.value)}
-							placeholder="Type your message here"
-							className="w-[90%]  bg-transparent border-none pl-4 text-xl text-[#585858] focus:outline-none"
-						/>
-						<button
-							type="submit"
-							className="submit-btn md:py-0 px-8 lg:py-1 rounded-[2rem] centered  border-none"
-						>
-							<IconSend2 color="#585858" size={32} />
-						</button>
-					</>
-				) : (
-					<>
-						<select name="" id="">
-							<option value="BTC">BTC</option>
-							<option value="ETH">ETH</option>
-							<option value="Doge">Doge</option>
-						</select>
-
-						<input
-							type="Number"
-							value={signal.buyPrice}
-							onChange={(e) => setMessage(e.target.value)}
-							placeholder="Type the price to buy"
-							className="w-[90%]  bg-transparent border-none pl-4 text-xl text-[#585858] focus:outline-none"
-						/>
-
-						<input
-							type="Number"
-							value={signal.sellPrice}
-							onChange={(e) => setMessage(e.target.value)}
-							placeholder="Type the price to sell"
-							className="w-[90%]  bg-transparent border-none pl-4 text-xl text-[#585858] focus:outline-none"
-						/>
-
-						<button
-							type="submit"
-							className="submit-btn md:py-0 px-8 lg:py-1 rounded-[2rem] centered  border-none"
-						>
-							Enviar
-						</button>
-					</>
-				)}
-			</form>
+			)}
 		</div>
 	);
+}
+
+{
+	/* <>
+	<select name="" id="">
+		<option value="BTC">BTC</option>
+		<option value="ETH">ETH</option>
+		<option value="Doge">Doge</option>
+	</select>
+
+	<input
+		type="Number"
+		value={signal.buyPrice}
+		onChange={(e) => setMessage(e.target.value)}
+		placeholder="Type the price to buy"
+		className="w-[90%]  bg-transparent border-none pl-4 text-xl text-[#585858] focus:outline-none"
+	/>
+
+	<input
+		type="Number"
+		value={signal.sellPrice}
+		onChange={(e) => setMessage(e.target.value)}
+		placeholder="Type the price to sell"
+		className="w-[90%]  bg-transparent border-none pl-4 text-xl text-[#585858] focus:outline-none"
+	/>
+
+	<button
+		type="submit"
+		className="submit-btn md:py-0 px-8 lg:py-1 rounded-[2rem] centered  border-none"
+	>
+		Enviar
+	</button>
+</>; */
 }
