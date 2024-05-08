db3c40e1a580f3c428173a16c0ea25200229dc5a
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-08 19:47:38 +0200

    Hacer el schema

--- client/src/components/SignalForm/SignalFormSchema.js
+++ client/src/components/SignalForm/SignalFormSchema.js
@@ -1,7 +1,11 @@
 import * as yup from "yup";
 
 export const SignalFormSchema = yup.object().shape({
-	username: yup.string().required("Tienes que elejir una moneda"),
-	buyPrice: yup.number().required("Buy Price Required"),
-	sellPrice: yup.number().required("Sell Price Required"),
+	signalImage: yup.mixed(),
+	description: yup.string(),
+	coin: yup.string().required("Tienes que elejir una moneda"),
+	entrada: yup.number().positive().required("La entrada es necesaria"),
+	salida: yup.number().positive().required("La salida es necesaria"),
+	tp: yup.number().positive().required("El tp es necesario"),
+	porcentaje: yup.number().positive().required("El porcentaje es necesario"),
 });
