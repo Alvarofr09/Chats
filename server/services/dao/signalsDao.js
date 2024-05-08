890da997d4e2eada2a3d77d3f5f63b38ccf7fa0e
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-08 19:49:06 +0200

    Dao de señales

---
+++ server/services/dao/signalsDao.js
@@ -0,0 +1,41 @@
+const moment = require("moment");
+const db = require("../db");
+const { removeUndefinedKeys } = require("../../utils/removeUndefinedKeys");
+
+const signalDao = {};
+
+signalDao.addSignal = async (signalData) => {
+	let conn = null;
+	try {
+		conn = await db.createConection();
+
+		const signalObj = {
+			sender_id: signalData.from,
+			group_id: signalData.to,
+			image: signalData.image,
+			description: signalData.description,
+			moneda: signalData.moneda,
+			entrada: signalData.entrada,
+			salida: signalData.salida,
+			tp: signalData.tp,
+			porcentaje: signalData.porcentaje,
+			date: moment().format("YYYY-MM-DD HH:mm:ss"),
+		};
+
+		// Eliminamos los campos que no se van a mofificar
+		signalObj = await removeUndefinedKeys(signalObj);
+
+		return await db.query(
+			"INSERT INTO signals SET ?",
+			signalObj,
+			"insert",
+			conn
+		);
+	} catch (error) {
+		throw new Error(error);
+	} finally {
+		conn && (await conn.end());
+	}
+};
+
+module.exports = signalDao;
