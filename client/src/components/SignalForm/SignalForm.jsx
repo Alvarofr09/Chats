3d77b69f67e9beb62177d9af8801f4930790c494
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-05-08 19:47:30 +0200

    Cambiar cosas del formulario de señales

--- client/src/components/SignalForm/SignalForm.jsx
+++ client/src/components/SignalForm/SignalForm.jsx
@@ -4,6 +4,7 @@ import { SignalFormSchema } from "./SignalFormSchema";
 
 import Input from "../ui/Input";
 import Select from "../ui/Select";
+import Image from "../ui/Image";
 
 export default function SignalForm() {
 	async function onSubmit(values) {
@@ -26,19 +27,25 @@ export default function SignalForm() {
 				{(values, errors, isSubmitting) => (
 					<div className="">
 						<Form className="form">
-							<Select
-								label="Moneda"
-								name="coin"
-								placeholder="Please select a coin"
-							>
+							<Image />
+
+							<Input
+								placeholder="Signal Description"
+								name="description"
+								type="textarea"
+							/>
+
+							<Select name="coin" placeholder="Please select a coin">
 								<option value="">Please select a coin</option>
 								<option value="BTC">BTC</option>
 								<option value="ETH">ETH</option>
 								<option value="Doge">Doge</option>
 							</Select>
 
-							<Input placeholder="Buy Price" name="buyPrice" type="number" />
-							<Input placeholder="Sell Price" name="sellPrice" type="number" />
+							<Input placeholder="Entrada" name="entrada" type="number" />
+							<Input placeholder="Salida" name="salida" type="number" />
+							<Input placeholder="TP" name="tp" type="number" />
+							<Input placeholder="%" name="porcentaje" type="number" />
 
 							<button
 								className="btn-primary"
